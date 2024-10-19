5f5d77134ea96d6a0c4ee118b25e4f5749636cdd
Ldrex1 <damzealwini7@gmail.com>
2024-10-19 01:57:42 +0100

    update: sg ids outputs

--- infra/modules/network/vpc/outputs.tf
+++ infra/modules/network/vpc/outputs.tf
@@ -1,3 +1,27 @@
 output "vpc_arn" {
   value = aws_vpc.k8_vpc.arn
 }
+output "vpc_id" {
+  value = aws_vpc.k8_vpc.id
+}
+
+output "k8_subnet_id" {
+  value = aws_subnet.k8_cluster.id
+}
+
+
+output "k8_security_group_ids" {
+  description = "All security groups in the k8 cluster"
+
+  value = [
+    aws_security_group.k8_master_node.id,
+    aws_security_group.k8_worker_node.id,
+  ]
+}
+output "k8_master_security_group_id" {
+  value = aws_security_group.k8_master_node
+}
+
+output "bastion_subnet_id" {
+  value = aws_subnet.bastion.id
+}
