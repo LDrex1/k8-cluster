1fa9aa94b4d28dc03935799b8920f22309b344ce
Ldrex1 <damzealwini7@gmail.com>
2024-10-19 01:56:59 +0100

    update: adding sgs and sg rules to the VPC section of the network module.

--- infra/modules/network/vpc/main.tf
+++ infra/modules/network/vpc/main.tf
@@ -23,9 +23,9 @@ resource "aws_network_acl" "k8_vpc" {
 }
 
 # Vpc resource
-resource "aws_vpc" "k8_vpc"{
-    cidr_block = var.k8_vpc_cidr
-    tags = {
+resource "aws_vpc" "k8_vpc" {
+  cidr_block = var.k8_vpc_cidr
+  tags = {
     Name = "Ter-k8"
   }
 }
@@ -35,7 +35,7 @@ resource "aws_internet_gateway" "gateway" {
   vpc_id = aws_vpc.k8_vpc.id
 
   tags = {
-    Name = "internet-gateway-for-k8-vpc"
+    Name   = "internet-gateway-for-k8-vpc"
     Source = "Terraform"
   }
 }
@@ -46,21 +46,20 @@ resource "aws_route" "route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gateway.id
 
-  
+
 }
 
 data "aws_availability_zones" "available" {}
 
 # Subnet definition for master and worker nodes. 
 resource "aws_subnet" "k8_cluster" {
-  count                   = 1
   vpc_id                  = aws_vpc.k8_vpc.id
-  cidr_block              = "10.0.${count.index}.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = false
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, 0)
 
   tags = {
-    Name = "k8-nodes-subnet-${element(data.aws_availability_zones.available.names, count.index)}"
+    Name   = "k8-nodes-subnet-${element(data.aws_availability_zones.available.names, 0)}"
     Source = "Terraform"
 
   }
@@ -68,14 +67,13 @@ resource "aws_subnet" "k8_cluster" {
 
 # Subnet definition for baston host. 
 resource "aws_subnet" "bastion" {
-  count                   = 1
   vpc_id                  = aws_vpc.k8_vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = element(data.aws_availability_zones.available.names, 1)
 
   tags = {
-    Name = "bastion-host-subnet-${element(data.aws_availability_zones.available.names, 1)}"
+    Name   = "bastion-host-subnet-${element(data.aws_availability_zones.available.names, 1)}"
     Source = "Terraform"
 
   }
@@ -83,35 +81,68 @@ resource "aws_subnet" "bastion" {
 
 
 resource "aws_security_group" "k8_master_node" {
-  name = "k8_master_sg"
+  name   = "k8_master_sg"
   vpc_id = aws_vpc.k8_vpc.id
 
+
+
   tags = {
-    Name = "k8_master_sg"
+    Name   = "k8_master_sg"
+    Source = "Terraform"
+
+  }
+}
+resource "aws_security_group" "k8_worker_node" {
+  name   = "k8_worker_sg"
+  vpc_id = aws_vpc.k8_vpc.id
+
+
+
+  tags = {
+    Name   = "k8_worker_sg"
     Source = "Terraform"
 
   }
 }
 
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.k8_master_node.id
-  cidr_ipv4         ="0.0.0.0/0"
+
+  for_each = {
+    k8_master_node = aws_security_group.k8_master_node.id,
+    k8_worker_node = aws_security_group.k8_worker_node.id,
+  }
+  depends_on = [aws_security_group.k8_master_node,
+  aws_security_group.k8_worker_node, ]
+
+  security_group_id = each.value
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
-  ip_protocol       ="TCP"
+  ip_protocol       = "TCP"
   to_port           = 22
+
+  tags = { Name : "SSH rule" }
+
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.k8_master_node.id
-  cidr_ipv4         ="0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
-  ip_protocol       ="TCP"
+  ip_protocol       = "TCP"
   to_port           = 443
+  depends_on = [aws_security_group.k8_master_node,
+  aws_security_group.k8_worker_node, ]
+
+  tags = { Name : "Https" }
+
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_k8" {
-  security_group_id = aws_security_group.k8_master_node.id
-  cidr_ipv4         ="0.0.0.0/0"
+  security_group_id = aws_security_group.k8_worker_node.id
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 30000
-  ip_protocol       ="TCP"
+  ip_protocol       = "TCP"
   to_port           = 32677
+  depends_on = [aws_security_group.k8_master_node,
+  aws_security_group.k8_worker_node, ]
 }
 
