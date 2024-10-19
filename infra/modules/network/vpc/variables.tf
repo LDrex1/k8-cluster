88b097644ad803fd99ec7f540a5dd00747a9f56d
Ldrex1 <damzealwini7@gmail.com>
2024-10-19 01:58:43 +0100

    update: k8 vpc tag default value update

--- infra/modules/network/vpc/variables.tf
+++ infra/modules/network/vpc/variables.tf
@@ -1,12 +1,12 @@
-variable "k8_vpc_cidr"{
-    type =  string
-    description= "Default cidr range for created VPCs"
-    default ="10.0.0.0/16"
+variable "k8_vpc_cidr" {
+  type        = string
+  description = "Default cidr range for created VPCs"
+  default     = "10.0.0.0/16"
 }
 
 variable "k8_vpc_tags" {
-    type =  map
-default = {name = "main-vps",
- function= "k8-cluster"
-}
+  type = map(any)
+  default = { name = "main-vpc",
+    function = "k8-cluster"
+  }
 }
