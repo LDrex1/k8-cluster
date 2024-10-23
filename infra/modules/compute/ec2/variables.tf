c395ab3f2d8db18da093f464a9a44fc9eb5dcce9
Ldrex1 <damzealwini7@gmail.com>
2024-10-23 18:27:16 +0100

    new var: sg id types

--- infra/modules/compute/ec2/variables.tf
+++ infra/modules/compute/ec2/variables.tf
@@ -12,3 +12,18 @@ variable "bastion_subnet_id" {
   description = "Subnet ID for bastion host"
   type        = string
 }
+
+variable "k8_master_sg_id" {
+  description = "Security group for master node"
+  type        = string
+
+}
+variable "k8_worker_security_group_id" {
+  description = "Security group for worker node"
+  type        = string
+}
+variable "bastion_security_group_id" {
+  description = "Security group for bastion host"
+  type        = string
+}
+
