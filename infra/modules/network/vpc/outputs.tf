e5a782fbb07c936e5179257aca1fb165b085ac95
Ldrex1 <damzealwini7@gmail.com>
2024-10-23 18:31:11 +0100

    output: sg ids

--- infra/modules/network/vpc/outputs.tf
+++ infra/modules/network/vpc/outputs.tf
@@ -10,16 +10,18 @@ output "k8_subnet_id" {
 }
 
 
-output "k8_security_group_ids" {
-  description = "All security groups in the k8 cluster"
+output "k8_master_sg_id" {
+  description = "Security group for master node"
+  value       = aws_security_group.k8_master_node.id
 
-  value = [
-    aws_security_group.k8_master_node.id,
-    aws_security_group.k8_worker_node.id,
-  ]
 }
-output "k8_master_security_group_id" {
-  value = aws_security_group.k8_master_node
+output "k8_worker_security_group_id" {
+  description = "Security group for worker node"
+  value       = aws_security_group.k8_worker_node.id
+}
+output "bastion_security_group_id" {
+  description = "Security group for bastion host"
+  value       = aws_security_group.bastion_web_app.id
 }
 
 output "bastion_subnet_id" {
