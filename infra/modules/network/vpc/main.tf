028c4735523e5548b6cc4e2b4af9a6db4174138a
Ldrex1 <damzealwini7@gmail.com>
2024-10-25 00:07:36 +0100

    added resources: nat gateway, private and public route tables

--- infra/modules/network/vpc/main.tf
+++ infra/modules/network/vpc/main.tf
@@ -39,14 +39,68 @@ resource "aws_internet_gateway" "gateway" {
     Source = "Terraform"
   }
 }
+resource "aws_eip" "k8_eip" {
+  depends_on = [ aws_internet_gateway.gateway]
+}
+resource "aws_nat_gateway" "k8_nat_gateway" {
+  subnet_id = aws_subnet.bastion.id
+  allocation_id = aws_eip.k8_eip.id
+
+  tags = {
+    Name = "k8_nat_gateway"
+    Source = "Terraform"
+  }
+
+  depends_on = [ aws_internet_gateway.gateway ]
+}
 
 # Route table
-resource "aws_route" "route" {
-  route_table_id         = aws_vpc.k8_vpc.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gateway.id
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.k8_vpc.id
+
+  route {
+    cidr_block       = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.k8_nat_gateway.id
+      }
+
+  tags = {
+    Name   = "private-route-table"
+    Source = "Terraform"
+  }
+}
+
+resource "aws_route_table_association" "private_route_association" {
+  subnet_id      = aws_subnet.k8_cluster.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.k8_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id   = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+    Source = "Terraform"
+  }
+}
 
 
+# resource "aws_route" "route" {
+#   route_table_id         = aws_vpc.k8_vpc.main_route_table_id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.gateway.id
+
+
+# }
+
+resource "aws_route_table_association" "public_route_association" {
+  subnet_id      = aws_subnet.bastion.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 data "aws_availability_zones" "available" {}
@@ -138,13 +192,17 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.k8_master_node.id
+    for_each = {
+    k8_master_node          = aws_security_group.k8_master_node.id,
+    Bastion_host_for_webapp = aws_security_group.bastion_web_app.id
+  }
+  security_group_id = each.value
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "TCP"
   to_port           = 443
   depends_on = [aws_security_group.k8_master_node,
-  aws_security_group.k8_worker_node, ]
+  aws_security_group.bastion_web_app, ]
 
   tags = { Name : "Https" }
 
@@ -166,5 +224,33 @@ resource "aws_vpc_security_group_egress_rule" "allow_outbound_ssh" {
   to_port           = 22
 
   tags = { Name : "SSH rule" }
+}
+resource "aws_vpc_security_group_egress_rule" "allow_outbound_https" {
+    for_each = {
+    k8_master_node          = aws_security_group.k8_master_node.id,
+    k8_worker_node          = aws_security_group.k8_worker_node.id,
+    Bastion_host_for_webapp = aws_security_group.bastion_web_app.id
+  }
+  security_group_id = each.value
+   cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "TCP"
+  to_port           = 443
+
+  tags = { Name : "https rule" }
+}
+resource "aws_vpc_security_group_egress_rule" "allow_outbound_http" {
+    for_each = {
+    k8_master_node          = aws_security_group.k8_master_node.id,
+    k8_worker_node          = aws_security_group.k8_worker_node.id,
+    Bastion_host_for_webapp = aws_security_group.bastion_web_app.id
+  }
+  security_group_id = each.value
+   cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "TCP"
+  to_port           = 80
+
+  tags = { Name : "http rule" }
 }
 
