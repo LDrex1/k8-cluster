bc17b1f635beeaa09500727810ef6f939a03c791
Ldrex1 <damzealwini7@gmail.com>
2025-01-17 04:54:27 +0100

    update: setting window condition to allow for SSR

--- nextjs/src/app/context/AuthContext.js
+++ nextjs/src/app/context/AuthContext.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { createContext, useState } from "react";
 import axios from "axios";
@@ -7,37 +7,47 @@ import { useRouter } from "next/navigation";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const router = useRouter();
+  const [user, setUser] = useState(null);
+  const router = useRouter();
 
-    const login = async (username, password) => {
-        try {
-            const formData = new FormData();
-            formData.append('username', username);
-            formData.append('password', password);
-            const response = await axios.post('http://localhost:8000/auth/token', formData, {
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            });
-            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`;
-            localStorage.setItem('token', response.data.access_token);
-            setUser(response.data);
-            router.push('/');
-        } catch (error) {
-            console.log('Login Failed:', error);
+  const login = async (username, password) => {
+    try {
+      const formData = new FormData();
+      formData.append("username", username);
+      formData.append("password", password);
+      const response = await axios.post(
+        "http://localhost:8000/auth/token",
+        formData,
+        {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
         }
-    };
+      );
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.access_token}`;
+      if (window) {
+        window.localStorage.setItem("token", response.data.access_token);
+        setUser(response.data);
+        router.push("/");
+      } else {
+        throw "can not find local storage";
+      }
+    } catch (error) {
+      console.log("Login Failed:", error);
+    }
+  };
 
-    const logout = () => {
-        setUser(null);
-        delete axios.defaults.headers.common['Authorization'];
-        router.push('/login')
-    };
+  const logout = () => {
+    setUser(null);
+    delete axios.defaults.headers.common["Authorization"];
+    router.push("/login");
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, login, logout}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
